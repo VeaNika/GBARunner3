b70a0ef17eae9f1681007a3adc11f27a114c7efe
VeaNika <veronika.sonya90@gmail.com>
2025-05-05 19:12:47 -0600

    Added Slot2 and Rumble drivers from BlocksDS

---
+++ code/core/arm9/source/Peripherals/RomGpio/Slot2Rumble/Rumble.c
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: Zlib
+//
+// Copyright (c) 2023 Adrian "asie" Siekierka
+
+#include <nds/ndstypes.h>
+#include <nds/memory.h>
+#include <nds/system.h>
+#include <nds/arm9/rumble.h>
+
+bool isRumbleInserted(void)
+{
+    if (!peripheralSlot2IsDetected())
+        rumbleInit();
+    return peripheralSlot2GetSupportMask() & SLOT2_PERIPHERAL_RUMBLE_ANY;
+}
+
+uint8_t rumbleGetMaxRawStrength(void)
+{
+    uint32_t mask = peripheralSlot2GetSupportMask();
+    if (mask & SLOT2_PERIPHERAL_RUMBLE_EZ)
+        return 3;
+    if (mask & SLOT2_PERIPHERAL_RUMBLE_ANY)
+        return 1;
+    return 0;
+}
+
+bool rumbleIsEdgeActivated(void)
+{
+    return peripheralSlot2GetSupportMask() & SLOT2_PERIPHERAL_RUMBLE_PAK;
+}
+
+#define RUMBLE_PAK_CTRL (*(vuint16 *)0x08001000)
+#define GPIO_DATA       (*(vuint16 *)0x080000C4)
+extern void __libnds_slot2EzCommand(uint32_t address, uint16_t value);
+static const uint8_t ez_rumble_table[] = {0x08, 0xF0, 0xF2, 0xF1};
+
+void setRumble(uint8_t strength)
+{
+    uint32_t mask = peripheralSlot2GetSupportMask();
+    peripheralSlot2Open(SLOT2_PERIPHERAL_RUMBLE_ANY);
+
+    if (mask & SLOT2_PERIPHERAL_RUMBLE_GPIO)
+    {
+        GPIO_DATA = (GPIO_DATA & ~0x8) | (strength ? 0x8 : 0x0);
+    }
+    else if (mask & SLOT2_PERIPHERAL_RUMBLE_PAK)
+    {
+        if (mask & SLOT2_PERIPHERAL_RUMBLE_EZ)
+        {
+            __libnds_slot2EzCommand(0x9E20000, ez_rumble_table[strength > 3 ? 3 : strength]);
+        }
+        RUMBLE_PAK_CTRL = strength ? 0x2 : 0x0;
+    }
+    else if (mask & SLOT2_PERIPHERAL_SLIDE_MAGKID)
+    {
+        // TODO: Untested.
+        RUMBLE_PAK_CTRL = strength ? 0x100 : 0x000;
+    }
+}
---
+++ code/core/arm9/source/Peripherals/RomGpio/Slot2Rumble/Rumble.h
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: Zlib
+//
+// Copyright (C) 2023 Adrian "asie" Siekierka
+
+#ifndef LIBNDS_NDS_ARM9_RUMBLE_H__
+#define LIBNDS_NDS_ARM9_RUMBLE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/// @file nds/arm9/rumble.h
+///
+/// @brief Slot-2 Rumble.
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <nds/arm9/peripherals/slot2.h>
+
+/// Initialize the rumble device.
+///
+/// @deprecated
+///     Use of peripheralSlot2Init() is recommended instead.
+static inline void rumbleInit(void)
+{
+    peripheralSlot2InitDefault();
+}
+
+/// Check if a rumble device has been inserted.
+///
+/// @return
+///     True if a rumble device has been inserted, false otherwise.
+bool isRumbleInserted(void);
+
+/// Check if a rumble device is edge-activated.
+///
+/// An edge-activated rumble device triggers its actuator when calling
+/// setRumble(1) if setRumble(0) was called prior.
+///
+/// A non-edge-activated rumble device triggers its motor starting from
+/// when setRumble(1) was called, until setRumble(0) is called.
+///
+/// @return
+///     It returns true if the device is edge-activated.
+bool rumbleIsEdgeActivated(void);
+
+/// Get the maximum rumble strength.
+///
+/// @return
+///     The maximum rumble strength for this device.
+uint8_t rumbleGetMaxRawStrength(void);
+
+/// Set the rumble device enable/disable pin.
+///
+/// Note that rumbleEnable() and rumbleDisable() are more user-friendly.
+///
+/// @param rawStrength
+///     The raw rumble strength (rumbleGetMaxRawStrength()).
+///
+/// @see rumbleIsEdgeActivated
+void setRumble(uint8_t rawStrength);
+#define RUMBLE_STRENGTH_HIGHEST 0xFF
+
+/// Emit a single, fast rumble tick.
+///
+/// @param rawStrength
+///     The raw rumble strength (rumbleGetMaxRawStrength()).
+static inline void rumbleTick(uint8_t rawStrength)
+{
+    setRumble(rawStrength);
+    setRumble(0);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LIBNDS_NDS_ARM9_RUMBLE_H__
