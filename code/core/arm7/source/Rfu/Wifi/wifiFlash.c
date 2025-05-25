9d62131f02e71d1d00c51665d0ae86110260708b
VeaNika <veronika.sonya90@gmail.com>
2025-05-25 02:26:01 -0600

    Updated SPICNT definitions

--- code/core/arm7/source/Rfu/Wifi/wifiFlash.c
+++ code/core/arm7/source/Rfu/Wifi/wifiFlash.c
@@ -10,19 +10,19 @@ void wifi_initFlashData()
     u32 oldIrq = rtos_disableIrqs();
     {
         //Read command
-        spi_transferByte(SPICNT_DEVICE_FIRMWARE | SPICNT_SPEED_4_MHZ, NVRAM_CMD_READ);
+        spi_transferByte(SPICNT_DEVICE_FLASH | SPICNT_SPEED_4_MHZ, NVRAM_CMD_READ);
 
         //Address
-        spi_transferByte(SPICNT_DEVICE_FIRMWARE | SPICNT_SPEED_4_MHZ, (0 >> 16) & 0xFF);
-        spi_transferByte(SPICNT_DEVICE_FIRMWARE | SPICNT_SPEED_4_MHZ, (0 >> 8) & 0xFF);
-        spi_transferByte(SPICNT_DEVICE_FIRMWARE | SPICNT_SPEED_4_MHZ, (0) & 0xFF);
+        spi_transferByte(SPICNT_DEVICE_FLASH | SPICNT_SPEED_4_MHZ, (0 >> 16) & 0xFF);
+        spi_transferByte(SPICNT_DEVICE_FLASH | SPICNT_SPEED_4_MHZ, (0 >> 8) & 0xFF);
+        spi_transferByte(SPICNT_DEVICE_FLASH | SPICNT_SPEED_4_MHZ, (0) & 0xFF);
 
         u16* dst = (u16*)&WIFI_RAM->firmData;
         //Read 2 bytes a time, because wifi ram does not support byte writes
         for(int i = 0; i < 512; i += 2) 
         {            
-            u8 a = spi_transferByte(SPICNT_DEVICE_FIRMWARE | SPICNT_SPEED_4_MHZ, 0);
-            u8 b = spi_transferByte(SPICNT_DEVICE_FIRMWARE | SPICNT_SPEED_4_MHZ, 0);
+            u8 a = spi_transferByte(SPICNT_DEVICE_FLASH | SPICNT_SPEED_4_MHZ, 0);
+            u8 b = spi_transferByte(SPICNT_DEVICE_FLASH | SPICNT_SPEED_4_MHZ, 0);
             *dst++ = a | (b << 8);
         }
 
